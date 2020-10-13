4853adf9d85f653da43baf8425d634c935dbdb9c
ihsakashi <yuvch122@gmail.com>
2020-10-13 13:55:19 -0600

    redo build script

--- devices/eon/build_usr/build_qt.sh
+++ devices/eon/build_usr/build_qt.sh
@@ -5,28 +5,26 @@ ROOT=$DIR/..
 QT_PACKAGE_VERSION=5.13.0
 QT_PACKAGE_URL="https://ftp.osuosl.org/pub/blfs/conglomeration/qt5/qt-everywhere-src-${QT_PACKAGE_VERSION}.tar.xz"
 
-echo "Building QT for NEOS"
-
-pushd $ROOT/mindroid/system
-    source build/envsetup.sh
-    breakfast oneplus3
-popd
-
-cd $ROOT/mindroid
-if [[ ! -d "$ROOT/mindroid/qt-everywhere-src-${QT_PACKAGE_VERSION}.tar.xz" ]]; then
-    wget --tries=inf $QT_PACKAGE_URL
-    tar xvf qt-everywhere-src-${QT_PACKAGE_VERSION}.tar.xz
-fi
-
 WORK_QT=$ROOT/mindroid/qt-everywhere-src-${QT_PACKAGE_VERSION}
 
-cp -rf $DIR/qt/qtbase/* ${WORK_QT}/qtbase/
+cd $ROOT/mindroid
 
-cd ${WORK_QT}
+if [[ ! -d "${WORK_QT}" ]]; then
+    wget --tries=inf $QT_PACKAGE_URL
+    tar xvf qt-everywhere-src-${QT_PACKAGE_VERSION}.tar.xz
 
-patch -p1 < $DIR/qt/patches/qt-everywhere-src-5.11.2_qtbase_src_network_kernel_qdnslookup_unix.cpp.patch
-patch -p1 < $DIR/qt/patches/qt-everywhere-src-5.11.2_qtbase_src_network_kernel_qhostinfo_unix.cpp.patch
-patch -p1 < $DIR/qt/patches/qt-everywhere-src-5.13.0_qtbase_src_plugins_platforms_eglfs_deviceintegration_deviceintegration.pro.patch
+    pushd ${WORK_QT}
+    patch -p1 < $DIR/qt/patches/qt-everywhere-src-5.11.2_qtbase_src_network_kernel_qdnslookup_unix.cpp.patch
+    patch -p1 < $DIR/qt/patches/qt-everywhere-src-5.11.2_qtbase_src_network_kernel_qhostinfo_unix.cpp.patch
+    patch -p1 < $DIR/qt/patches/qt-everywhere-src-5.13.0_qtbase_src_plugins_platforms_eglfs_deviceintegration_deviceintegration.pro.patch
+    popd
+
+    cp -rf $DIR/qt/qtbase/* ${WORK_QT}/qtbase/
+fi
+
+echo "Building QT for NEOS"
+
+#(cd $ROOT/mindroid/system && source build/envsetup.sh && breakfast oneplus3)
 
 cd qtbase
 
