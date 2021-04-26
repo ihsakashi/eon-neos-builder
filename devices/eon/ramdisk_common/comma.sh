4197759d81ad5f0c34bb42c3c01bcdbb42b53ba9
ihsakashi <yuvch122@gmail.com>
2021-04-26 17:41:43 -0600

    ramdisk: save sed changes

--- devices/eon/ramdisk_common/comma.sh
+++ devices/eon/ramdisk_common/comma.sh
@@ -60,7 +60,7 @@ fi
 
 if ! grep -q "event2" "/system/comma/home/.bash_profile"; then
   mount -o rw,remount /system
-  sed "s|event1|event2|g" "/system/comma/home/.bash_profile"
+  sed -i "s|event1|event2|g" "/system/comma/home/.bash_profile"
   mount -o ro,remount /system
 fi
 
